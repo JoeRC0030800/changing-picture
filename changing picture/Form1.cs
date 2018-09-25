f8914b97b6c10277a7ab46deb18a4e2ccf3c1119
RC0030800 <RC0030800@CRO-24-15>
2018-09-25 10:44:05 +0100

    Started chaging pictures

--- changing picture/Form1.cs
+++ changing picture/Form1.cs
@@ -12,9 +12,23 @@ namespace changing_picture
 {
     public partial class Form1 : Form
     {
+        private Image dog;
+        private Image Dog2;
+
         public Form1()
         {
             InitializeComponent();
+            dog = Properties.Resources.dog;
+            Dog2 = Properties.Resources.Boxer_On_White_03_400x267;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == dog) ;
+            {
+                pictureBox1.Image = Dog2;
+            }
+            }
         }
     }
 }
